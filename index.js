979652fa572cff3435bca777b57d1cddeb1d6f91
user.name:"Jeevan neupane <user.email:"neupanejeevan446@gmail.com>
2025-03-21 07:44:44 +0545

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+
+const app = express();
+app.use(express.json());
+
+
+
+app.use(cookieParser());
+
+app.get("/data",(req,res)=>{
+    return res.send("Hello from backend server")
+})
+
+
+app.listen(8000,()=>{
+    console.log("app is listening at 8000 port");
+})
